f57705d625ba41520ed64895558475bdb9c97d9b
CS <syrious3000@yahoo.de>
2023-10-28 12:14:20 +0200

    update to 3.5

---
+++ common/src/test/java/org/acme/testcoverage/CommonServiceTest.java
@@ -0,0 +1,13 @@
+package org.acme.testcoverage;
+
+import org.junit.jupiter.api.Test;
+
+public class CommonServiceTest {
+
+    @Test
+    public void testGreetingService() {
+// commented as CommonService should be covered by org.acme.testcoverage.GreetingServiceTest
+//        CommonService commonService = new CommonService();
+//        commonService.greeting("bla");
+    }
+}
