49e74bd52819fe7f3ce62057ebedd02d3b016b46
Schaefer, Christian <C.Schaefer@t-systems.com>
2023-11-01 11:46:53 +0100

    aggregated coverage

--- common/src/test/java/org/acme/testcoverage/CommonServiceTest.java
+++ common/src/test/java/org/acme/testcoverage/CommonServiceTest.java
@@ -1,13 +1,15 @@
 package org.acme.testcoverage;
 
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+@QuarkusTest
 public class CommonServiceTest {
 
-    @Test
-    public void testGreetingService() {
-    // Shouldn't CommonService already be covered by org.acme.testcoverage.GreetingServiceTest (????)
-        CommonService commonService = new CommonService();
-        commonService.greeting("bla");
-    }
+//    @Test
+//    public void testGreetingService() {
+//    // Shouldn't CommonService already be covered by org.acme.testcoverage.GreetingServiceTest (????)
+//        CommonService commonService = new CommonService();
+//        commonService.greeting("bla");
+//    }
 }
--- service/src/test/java/org/acme/testcoverage/GreetingServiceTest.java
+++ service/src/test/java/org/acme/testcoverage/GreetingServiceTest.java
@@ -1,16 +1,21 @@
 package org.acme.testcoverage;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@QuarkusTest
 public class GreetingServiceTest {
+    @Inject
+    GreetingService service;
+
+    @Inject
+    CommonService commonService;
 
     @Test
     public void testGreetingService() {
-        GreetingService service = new GreetingService();
         Assertions.assertEquals("hello Quarkus", service.greeting("Quarkus"));
-
-        CommonService commonService = new CommonService();
         commonService.greeting("bla");
     }
 }
